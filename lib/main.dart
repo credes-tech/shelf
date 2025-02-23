d5e6a0c78177fd9f8e3d4a7488260d17c64e3bc9
credes.tech <credes.tech@gmail.com>
2025-02-23 12:54:13 +0530

    env-setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
@@ -22,6 +23,7 @@ void main() async {
 }
 
 setupDependencies() async {
+  await dotenv.load(fileName: ".env");
   await Hive.initFlutter();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
