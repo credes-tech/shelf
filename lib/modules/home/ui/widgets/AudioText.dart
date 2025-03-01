2d3605fc01b9ad218c7d55312d4457ac69989daf
akhilsiraswal <akhilsiraswal@gmail.com>
2025-03-01 23:44:28 +0530

    refactored audio

--- lib/modules/home/ui/widgets/AudioText.dart
+++ lib/modules/home/ui/widgets/AudioText.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 import 'package:my_shelf_project/core/theme/app_text_styles.dart';
@@ -16,18 +13,43 @@ class AudioText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Audio Name",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              // Text("hii"),
+              Expanded(
+                  child: Row(
+                children: [
+                  Text("waveform"),
+                ],
+              )),
+            ],
+          ),
+        ));
+
     return Marquee(
       text: audio.filename,
       style: AppTextStyles.audioTitle,
-      scrollAxis: Axis.horizontal,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      // scrollAxis: Axis.horizontal,
+      // crossAxisAlignment: CrossAxisAlignment.start,
       blankSpace: 20.0,
       velocity: 30.0,
-      pauseAfterRound: Duration(seconds: 1),
-      accelerationDuration: Duration(seconds: 1),
-      accelerationCurve: Curves.linear,
-      decelerationDuration: Duration(milliseconds: 500),
-      decelerationCurve: Curves.easeOut,
+      // pauseAfterRound: Duration(seconds: 1),
+      // accelerationDuration: Duration(seconds: 1),
+      // accelerationCurve: Curves.linear,
+      // decelerationDuration: Duration(milliseconds: 500),
+      // decelerationCurve: Curves.easeOut,
     );
   }
 }
