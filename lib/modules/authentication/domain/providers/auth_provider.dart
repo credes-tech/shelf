ffde26c560f582aa6d5a1518727985558f4eb890
Sachin Chaurasia <sachinchaurasia608@gmail.com>
2025-03-02 16:35:20 +0530

    Check IsLogin & IsOnboarded

--- lib/modules/authentication/domain/providers/auth_provider.dart
+++ lib/modules/authentication/domain/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/datasource/remote/firebase_service.dart';
 import '../../data/model/user_model.dart';
 import '../../data/repository/auth_repository_impl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
   return AuthRemoteDataSource();
@@ -19,26 +20,39 @@ final authStateProvider = StateNotifierProvider<AuthNotifier, UserModel?>((ref) 
 
 class AuthNotifier extends StateNotifier<UserModel?> {
   final AuthRepository _authRepository;
+  bool isOnboarded = false; // Stores onboarding state
 
-  AuthNotifier(this._authRepository) : super(_authRepository.getCurrentUser());
+  AuthNotifier(this._authRepository) : super(null) {
+    Future.microtask(() => _loadUserState());
+  }
 
   Future<void> signUp({required String email, required String password, required String name}) async {
     final user = await _authRepository.signUp(email: email, password: password, name: name);
+    if (user != null) {
+      await _saveLoginState(true);
+    }
     state = user;
   }
 
   Future<void> login({required String email, required String password}) async {
     final user = await _authRepository.login(email: email, password: password);
+    if (user != null) {
+      await _saveLoginState(true);
+    }
     state = user;
   }
 
   Future<void> googleSignIn() async {
     final user = await _authRepository.googleSignIn();
+    if(user != null) {
+      await _saveLoginState(true);
+    }
     state = user;
   }
 
   Future<void> logout() async {
     await _authRepository.logout();
+    await _saveLoginState(false);
     state = null;
   }
 
@@ -46,12 +60,28 @@ class AuthNotifier extends StateNotifier<UserModel?> {
     await _authRepository.sendPasswordResetEmail(email: email);
   }
 
-  void _getCurrentUser() {
-    state = _authRepository.getCurrentUser();
+  Future<void> _saveLoginState(bool isLoggedIn) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', isLoggedIn);
   }
 
-  // Public method
-  UserModel? getCurrentUser() {
-    return state;
+  Future<void> _loadUserState() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+    isOnboarded = prefs.getBool('isOnboarded') ?? false; // Load onboarding state
+
+    if (isLoggedIn) {
+      state = _authRepository.getCurrentUser();
+    }
+  }
+
+  Future<void> completedOnboarding() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isOnboarded', true);
+    isOnboarded = true;
+  }
+
+  bool getOnboardingStatus() {
+    return isOnboarded;
   }
 }
