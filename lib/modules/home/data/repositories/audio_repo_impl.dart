8ad171637bb3d78ccc2193bc623b1c194bc7d9b0
akhilsiraswal <akhilsiraswal@gmail.com>
2025-03-25 23:53:40 +0530

    added select multiple functionality in audio

--- lib/modules/home/data/repositories/audio_repo_impl.dart
+++ lib/modules/home/data/repositories/audio_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:my_shelf_project/modules/home/data/datasource/local/audio_hive_service.dart';
 import 'package:my_shelf_project/modules/home/data/models/audio_hive_model.dart';
+import 'package:my_shelf_project/modules/home/domain/models/audio_model.dart';
 
 class AudioRepository {
   final AudioHiveService _hiveService;
@@ -12,7 +13,10 @@ class AudioRepository {
 
   Future<void> deleteAudio(int index) => _hiveService.deleteAudio(index);
 
-  Future<bool> isAudioExists(String fileName) => _hiveService.isAudioExists(fileName);
+  Future<bool> isAudioExists(String fileName) =>
+      _hiveService.isAudioExists(fileName);
 
   Future<void> togglePin(String fileName) => _hiveService.togglePin(fileName);
+  Future<void> deleteMultipleAudios(List<String> selectedAudioList) =>
+      _hiveService.deleteMultipleAudios(selectedAudioList);
 }
